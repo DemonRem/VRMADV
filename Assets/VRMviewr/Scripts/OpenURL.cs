f2d9a9f5babdf25cad685c0ba9427dd4df92ae66
yahagi_day <yahagi.day@tensi.org>
2019-02-18 18:11:40 +0900

    Create OpenURL.cs

---
+++ Assets/VRMviewr/Scripts/OpenURL.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenURL : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    public void Link()
+    {
+        var url = text.GetComponent<Text>().text;
+        Debug.Log(url);
+        Application.OpenURL(url);
+    }
+}
