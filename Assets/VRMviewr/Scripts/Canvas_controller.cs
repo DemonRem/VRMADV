c2ed81415422523bfd82a11190bffa2aeb64c582
yahagi_day <yahagi.day@tensi.org>
2019-02-18 18:12:46 +0900

    Create Canvas_controller.cs

---
+++ Assets/VRMviewr/Scripts/Canvas_controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject canvas_prefab;
+    public void OnClickDo()
+    {
+        GameObject SelectUI =  Instantiate(canvas_prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    public void OnButtonYes()
+    {
+        Destroy(transform.root.gameObject);
+    }
+
+    public void OnButtonNo()
+    {
+        Destroy(transform.root.gameObject);
+    }
+
+}
